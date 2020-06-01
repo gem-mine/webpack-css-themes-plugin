c374cb6d87d9b434d38c0c1299e215754c978ee5
yaoj <yaoj@landicorp.com>
2020-06-01 17:40:20 +0800

    test: 现在允许主题文件使用非变量内容，重新设定测试案例

--- test/unit/cases/single-theme/less-theme-less-loader-not-variable-content/warnings.js
+++
@@ -1,3 +0,0 @@
-const path = require('path')
-
-module.exports = `WARNING in Webpack-css-themes-plugin themeFile ${path.resolve(__dirname, './theme/index.less')} has not-variable content which would be ignore`
