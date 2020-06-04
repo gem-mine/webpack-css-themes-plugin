34248668d17f7ed0528bb75d35acd896510e8ec8
yaoj <624975@nd.com>
2020-06-04 11:13:55 +0800

    chore: 新增注释，gitignore更精确

--- src/const.js
+++ src/const.js
@@ -1,4 +1,4 @@
 module.exports = {
   PluginName: 'webpack-css-theme-plugin',
-  MODULE_TYPE: 'css/mini-extract'
+  MODULE_TYPE: 'css/mini-extract' // to be compatible with mini-extract's other plugin
 }
